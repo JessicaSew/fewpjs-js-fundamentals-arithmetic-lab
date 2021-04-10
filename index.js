7fc227663c2f4eeebba0b166a97466196b3dea61
jessicasewell <sewell.jessica95@gmail.com>
2021-04-10 18:48:40 -0400

    Submitting assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+
+let num3 = 9;
+let num4 = 5;
+
+let multiply 
+let random
+let mod 
+let max 
+
+let min_random = 1
+let max_random = 4;
+
+multiply = num1 * num2
+random = Math.floor(Math.random() * 10)
+mod = num3 % num4
+max = Math.max(1, 5, 7, 20)
+
